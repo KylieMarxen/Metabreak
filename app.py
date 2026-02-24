96793a6e2dd5a7088b9bc7c732414e394f86d9bc
Kylie Marxen <KylieBriann.Marxen@calbaptist.edu>
2026-02-24 12:56:16 -0800

    Add database creation in app context

--- app.py
+++ app.py
@@ -854,5 +854,8 @@ def seed_db():
     db.session.commit()
     print('Database seeded!')
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
